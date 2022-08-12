958343d6eb35473ce76f99f5f78766241f61d005
jcartaza <100419928+jcartaza@users.noreply.github.com>
2022-08-12 16:04:04 -0300

    first commit

---
+++ src/components/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: 'tarea1',
+    description: 'mi primer tarea'
+  },
+  {
+    id: 1,
+    title: 'tarea2',
+    description: 'mi segunda tarea'
+  },
+  {
+    id: 2,
+    title: 'tarea3',
+    description: 'mi tercer tarea'
+  },
+];
